ba311c878761fbad61f1b2f8d0e32abc2a9c1d90
DESKTOP-2LVKBQD\user <mayhy28@chunjae.co.kr>
2022-06-28 14:15:45 +0900

    create Tsherpa

---
+++ js/script.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+    
+    $('head').load('head.html');
+    $('#header-loding').load('header.html');
+    $('#header-loding-login').load('header-login.html');
+
+    $('.select-box > a').on('click', function(e){
+        e.preventDefault();
+        if($('.select-box > .option-list').hasClass('up') === true) {
+            $('.option-list').removeClass('up');
+            $('.option-list').slideDown(300);
+        } else {
+            $('.option-list').addClass('up');
+            $('.option-list').slideUp(300);
+        }
+    }); 
+    
+    $('.stu-code-box > a').click(function(e){
+        e.preventDefault();
+        $('.popup-share').show();
+    });
+    $('.popup-header > a').click(function(e){
+        e.preventDefault();
+        $('.popup-share').hide();
+    });
+
+    $('.list-menu-group > a, .next-pagebtn-group > .page-num').on('click',function(e){
+        e.preventDefault();
+        if($(this).hasClass('active') === true) {
+            $(this).siblings().removeClass('active');
+        } else {
+            $(this).addClass('active');
+            $(this).siblings().removeClass('active');
+        }
+    });
+
+    $('.stu-pass-list > a').on('click',function(e){
+        e.preventDefault();
+        if($(this).hasClass('pass-show') === true) {
+            $(this).removeClass('pass-show');
+            $(this).addClass('pass-hide');
+        } else {
+            $(this).removeClass('pass-hide');
+            $(this).addClass('pass-show');
+        }
+    });
+    
+    var swiper = new Swiper(".class-offcial-box", {
+        spaceBetween: 50,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+    });
+})
