f7f526729f0a3f670dc6ebe21bb6afaae8e4307f
DESKTOP-2LVKBQD\user <mayhy28@chunjae.co.kr>
2022-07-12 16:51:05 +0900

    commit

--- js/script.js
+++ js/script.js
@@ -1,25 +1,6 @@
 $(document).ready(function(){
     // include 함수
-    includeLayout();
-
-    // 햄버거 메뉴 클릭시
-    $('.nav_btn').on('click', function(){
-        $('.menu-list-area').show()
-    });
-    $('.list-popup > a').on('click', function(){
-        $('.menu-list-area').hide()
-    });
-
-    $('.select-box > a').on('click', function(e){
-        e.preventDefault();
-        if($('.select-box > .option-list').hasClass('up') === true) {
-            $('.option-list').removeClass('up');
-            $('.option-list').slideDown(300);
-        } else {
-            $('.option-list').addClass('up');
-            $('.option-list').slideUp(300);
-        }
-    }); 
+    includeLayout(); 
     
     $('.stu-code-box > a').click(function(e){
         e.preventDefault();
